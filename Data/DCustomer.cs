80398cbec57681e82168652acb928d1dfd657efb
htorrico <htorrico@truenorthcorporation.com>
2025-10-06 19:05:25 -0500

    Add project files.

---
+++ Data/DCustomer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+
+namespace Data
+{
+    public class DCustomer
+    {
+
+        private string connectionString= "Server=HUGO\\SQLEXPRESS01;Database=INVOICESADB;Integrated Security=True; TrustServerCertificate=true";
+        public List<Customer> ListarCustomers()
+        {
+            List<Customer> customers = new List<Customer>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("sp_ListCustomers", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Customer c = new Customer
+                        {
+                            CustomerID = Convert.ToInt32(reader["customer_id"]),
+                            Name = reader["name"].ToString(),
+                            Address = reader["address"] != DBNull.Value ? reader["address"].ToString() : string.Empty,
+                            Phone = reader["phone"] != DBNull.Value ? reader["phone"].ToString() : string.Empty,
+                            Active = Convert.ToBoolean(reader["active"])
+                        };
+
+                        customers.Add(c);
+                    }
+                }
+            }
+
+            return customers;
+        }
+    }
+}
