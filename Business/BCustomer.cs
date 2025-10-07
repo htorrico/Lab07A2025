0feba15308e70f08d5752f75e1d28ba6d35ee4f7
htorrico <htorrico@truenorthcorporation.com>
2025-10-06 19:10:34 -0500

    business

---
+++ Business/BCustomer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Entity;
+
+
+namespace Business
+{
+    public class BCustomer
+    {
+        public List<Customer> ObtenerCustomers()
+        {
+
+         DCustomer _customerData = new DCustomer();
+         return _customerData.ListarCustomers();
+           
+        }
+    }
+}
